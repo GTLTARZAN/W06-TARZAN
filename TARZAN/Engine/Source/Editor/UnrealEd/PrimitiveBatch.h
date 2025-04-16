c1cb79f3937da204e5e64d651945487a663d859a
chanhuiHan <hch3245@naver.com>
2025-04-16 23:55:22 +0900

    feat: Directional Line Gizmo 구현

--- TARZAN/Engine/Source/Editor/UnrealEd/PrimitiveBatch.h
+++ TARZAN/Engine/Source/Editor/UnrealEd/PrimitiveBatch.h
@@ -26,12 +26,15 @@ public:
     void ReleaseOBBResources();
     void UpdateCircleResources();
     void ReleaseCircleResources();
+    void UpdateLineResources();
+    void ReleaseLineResources();
     void RenderAABB(const FBoundingBox& localAABB, const FVector& center, const FMatrix& modelMatrix);
     void RenderOBB(const FBoundingBox& localAABB, const FVector& center, const FMatrix& modelMatrix);
 	
     void AddCone(const FVector& Center, float Radius, float Angle, const FLinearColor& Color, const FMatrix& ModelMatrix);
     void AddCircle(const FVector& center, float radius, int segments, const FLinearColor& color, const FMatrix& modelMatrix);
-	
+    void AddLine(const FVector& start, const FVector& end, const FLinearColor& color);
+
     // 복사 생성자 및 대입 연산자 삭제
     UPrimitiveBatch(const UPrimitiveBatch&) = delete;
     UPrimitiveBatch& operator=(const UPrimitiveBatch&) = delete;
@@ -42,18 +45,23 @@ private:
     ID3D11Buffer* pConesBuffer;
     ID3D11Buffer* pCircleBuffer;
     ID3D11Buffer* pOBBBuffer;
+    ID3D11Buffer* pLineBuffer;
     ID3D11ShaderResourceView* pConesSRV;
     ID3D11ShaderResourceView* pOBBSRV;
     ID3D11ShaderResourceView* pCircleSRV;
+    ID3D11ShaderResourceView* pLineSRV;
 
     size_t allocatedBoundingBoxCapacity;
     size_t allocatedConeCapacity;
     size_t allocatedOBBCapacity;
     size_t allocatedCircleCapacity;
+    size_t allocatedLineCapacity;
     TArray<FBoundingBox> BoundingBoxes;
     TArray<FOBB> OrientedBoundingBoxes;
     TArray<FCone> Cones;
     TArray<FCircle> Circles;
+    TArray<FLine> Lines;
+
     FGridParameters GridParam;
     int ConeSegmentCount = 0;
     int CircleSegmentCount = 0;
